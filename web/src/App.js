54a8c955a8ad447fca4a0b42d1894ce717185b34
Avichail Benjamin <avichailbenjamin@gmail.com>
2019-01-27 12:00:49 +0200

    0.0.0

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hey
+      </div>
+    );
+  }
+}
+
+export default App;
